f3798062a5f242dbb0ac574c38d359624f8a7f05
gabrielcavalcante07 <bielcavalcante07@gmail.com>
2026-04-15 18:29:33 -0300

    chore: update dependencies and tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,26 @@ export default {
   theme: {
     extend: {
       colors: {
-        'background': '#1a1a1a',
+        'background': '#141414',
         'primary': '#915eff',
+        'blueIn': '#0a66c3'
       },
       fontFamily: {
-        'Beau-Rival': 'beau rivage, sans-serif',
-      }
+        inter: ['Inter', 'sans-serif'],
+      },
+      fontSize: {
+        'xs1': '0.85rem'
+      },
+      height:{
+        'rem': '25rem'
+      },
+      boxShadow: {
+        glass: 
+        `-2px -2px 2px 0px rgba(255, 255, 255, 0.28) inset,
+        2px 2px 2px 0px rgba(255, 255, 255, 0.28) inset,
+        0px 0px 19px 0px rgba(000, 000, 000, 0.25),
+        16px 0px 23.1px 0px rgba(0, 0, 0, 0.2)`
+      },
     },
   },
   plugins: [],
