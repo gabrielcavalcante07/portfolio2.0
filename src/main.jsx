a84d974467803a206782aa3327f5925ff472714b
gabrielcavalcante07 <bielcavalcante07@gmail.com>
2026-04-15 18:02:23 -0300

    feat(routes): add routing configuration

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App.jsx'
 import './global.css' 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { NotFound } from './pages/notFound.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>
+  },
+  {
+    path: '*',
+    element: <NotFound/>
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
